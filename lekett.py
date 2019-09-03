64adf61ea30ced2940a242242dac467b0546c945
carlfilipmodin <carlfilip.modin@learnet.se>
2019-09-03 10:09:37 +0200

    första 80 minuterna av lektionen tisdag 09-03

--- lekett.py
+++ lekett.py
@@ -1,1 +1,15 @@
-print("hej på dig snygging")
+print ("hello")
+firstName = "Calle"
+lastName = 'Modin'
+
+#DateOfBirth = "20000727"
+age = 19
+teacher =True
+
+print (firstName , lastName , (str)(age) + str(teacher)) 
+print (dateOfBirth) ""
+
+
+
+
+
