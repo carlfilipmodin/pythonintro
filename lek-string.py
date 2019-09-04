8a8e7bd0cbf93e6e3edac309ff78b25987b59a68
carlfilipmodin <carlfilip.modin@learnet.se>
2019-09-04 13:19:18 +0200

    lektion 19-09-04

---
+++ lek-string.py
@@ -0,0 +1,23 @@
+firstName = "carl-filip"
+middleName = "Alexander"
+lastName = "Modin"
+nicName = "N/A"  
+
+tecken = """
+ ()paranteser 
+ []hakparanteser
+ :kolon
+ ;semikolon 
+ ,komma 
+ \" dubblefnutt 
+ \' enkelfutt """  
+print (tecken[13:100])
+
+# för.efternamn
+# calle.modin
+# firstname[3].lastname
+print(firstName[0:4] + "." + lastName[0:4])
+print (lastName[0:3].lower() + "" + lastName[0:3].lower() + "19")
+userName = firstName[0:3].lower()
+userName += "19"
+print(userName)
