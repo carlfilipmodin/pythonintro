3e4df475998af6567f8f844d18d44e961832a2fa
carlfilipmodin <carlfilip.modin@learnet.se>
2019-09-10 10:46:58 +0200

    lektion3

--- lektre.py
+++ lektre.py
@@ -1,5 +1,29 @@
 print ("välkommen till mitt program där du kan addera")
-tal1=input("mata in ett heltal")
-tal2=input ("mata in ett till heltal: ")
-summa= int(tal1)+int(tal2)
-print ("summan är:" + str(summa))
+operator = input("välj räknesätt(+,-,*): ")
+try:
+     tal1= int(input("mata in ett heltal"))
+     tal2= int(input("mata in ett heltal"))
+except:
+    print ("du måste skriva in en siffra")
+    tal1 = 0
+    tal2 = 0
+if operator == "+":
+    summa = tal1 + tal2
+elif operator == "-":
+    summa="?"
+elif operator == "/":
+    summa= tal1/tal2
+    try:
+        tal1/tal2 
+    except ZeroDivisionError:
+      print("du kan inte dela med 0")
+elif operator == "*":
+    summa= tal1 * tal2
+
+else:
+    print("RÖR DEN INTE")
+print("summa är:" + str(summa))
+
+
+
+
